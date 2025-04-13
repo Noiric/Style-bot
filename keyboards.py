1fab3b9b405838703bd338fac810c4835e3d815f
Dima2024-python <chagaydapython2024@gmail.com>
2025-04-13 13:47:20 +0300

    04created_bot

--- keyboards.py
+++ keyboards.py
@@ -13,20 +13,44 @@ keyboard_back_1 = types.ReplyKeyboardMarkup(resize_keyboard=True).add(
 keyboard_2 = types.ReplyKeyboardMarkup(resize_keyboard=True).add(
     types.KeyboardButton(text='Самостійно'),
     types.KeyboardButton(text='Допомога продавця'),
-    types.KeyboardButton(text='На попередню сторінку')
+    types.KeyboardButton(text='На головну')
 )
 
 keyboard_back_2 = types.ReplyKeyboardMarkup(resize_keyboard=True).add(
     types.KeyboardButton(text='На попередню сторінку'),
-    types.KeyboardButton(text='На початок')
+    types.KeyboardButton(text='На головну')
+)
+
+keyboard_back_all = types.ReplyKeyboardMarkup(resize_keyboard=True).add(
+    types.KeyboardButton(text='До всіх товарів'),
+    types.KeyboardButton(text='На попередню сторінку'),
+    types.KeyboardButton(text='На головну')
 )
 
 keyboard_3 = types.ReplyKeyboardMarkup(resize_keyboard=True).add(
     types.KeyboardButton(text='Всі товари'),
     types.KeyboardButton(text='Пошук'),
     types.KeyboardButton(text='На попередню сторінку'),
-    types.KeyboardButton(text='На початок')
+    types.KeyboardButton(text='На головну')
 )
-# keyboard_search =
-# keyboard_all_products =
+keyboard_search = types.ReplyKeyboardMarkup(resize_keyboard=True).add(
+    types.KeyboardButton(text='За сезонністю'),
+    types.KeyboardButton(text='За типом одягу'),
+    types.KeyboardButton(text='За ціною'),
+    types.KeyboardButton(text='За назвою'),
+    types.KeyboardButton(text='На попередню сторінку'),
+    types.KeyboardButton(text='На головну')
+)
 
+keyboard_all_products = types.ReplyKeyboardMarkup(resize_keyboard=True).add(
+                           types.KeyboardButton(text='Замовити'),
+                           types.KeyboardButton(text='Пошук'),
+                           types.KeyboardButton(text='Допомога'),
+                           types.KeyboardButton(text='На попередню сторінку'),
+                           types.KeyboardButton(text='На головну')
+)
+
+buy_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True).add(
+    types.KeyboardButton(text='На попередню сторінку'),
+    types.KeyboardButton(text='На головну')
+)
